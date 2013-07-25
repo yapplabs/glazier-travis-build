744f6237a71670685580a8054c7c4124c7ee43a0
kagemusha <mmadrid99@gmail.com>
2013-07-25 10:21:34 -0400

    refactor travis api calls

--- app/models/build.js
+++ app/models/build.js
@@ -12,33 +12,46 @@ var Build = {
    */
   //https://api.travis-ci.org/repos/yapplabs/glazier/builds
   findBuildsByRepositoryName: function(repositoryName) {
-    console.log("findBuildsByRepositoryName: get travis builds")
-    var service = card.consumers.fullXhr;
-
-    return service.request("ajax", {
-      url: card.baseTravisUrl + '/repos/' + repositoryName + '/builds',
-      type: 'get',
-      dataType: 'json'
-    });
+    var url = '/repos/' + repositoryName + '/builds';
+    return this.getTravisData(url);
   },
 
   /*
    @public
 
-   Fetches the current issues given a repository name.
+   Fetches the build with the given id for the given repository.
 
-   @method findMineByRepositoryName
+   @method findMineByRepositoryNameAndId
    @param  repositoryName {String}
+   @param  buildId {String}
    @returns {Ember.RSVP.Promise}
    */
   //https://api.travis-ci.org/repos/yapplabs/glazier/builds/9271771
-  findBuildByRepositoryNameAndId: function(repositoryName, creator) {
-//    return card.consumers.authenticatedGithubApi.request("ajax", {
-//      url: '/repos/' + repositoryName + '/issues?creator=' + creator,
-//      dataType: 'json'
-//    });
-    return [];  //todo
+  findBuildByRepositoryNameAndId: function(repositoryName, buildId) {
+    var url = '/repos/' + repositoryName + '/builds/' + buildId;
+    return this.getTravisData(url);
+  },
+
+
+  /*
+   @public
+
+   Makes a get call to the travis api with the relative url.
+
+   @method getTravisData
+   @param  relativeUrl {String}
+   @returns {Ember.RSVP.Promise}
+   */
+  getTravisData: function(relativeUrl) {
+    var service = card.consumers.fullXhr;
+
+    return service.request("ajax", {
+      url: card.baseTravisUrl + '/' + relativeUrl,
+      type: 'get',
+      dataType: 'json'
+    });
   }
+
 };
 
 export default Build;
