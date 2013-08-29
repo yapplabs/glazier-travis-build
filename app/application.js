5fcd2940740e6d275048eb105237f358071c1ca1
Ray Cohen <raycohen@gmail.com>
2013-08-29 17:47:29 -0400

    update usage of Application.Resolver for Ember rc 8

--- app/application.js
+++ app/application.js
@@ -3,7 +3,7 @@ import Resolver from 'resolver';
 var App = Ember.Application.create({
   modulePrefix: 'app',
   rootElement: '#card',
-  resolver: Resolver
+  Resolver: Resolver
 });
 
 App.deferReadiness();
